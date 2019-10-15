b9b94c918320d9ad6b0406498bd0b7d380ee7952
tianchizhishui <davidliutianchi@gmail.com>
2019-10-14 22:30:18 -0700

    helloWorld java code

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello David");
+
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myFirstNumber = (10 + 5) + (8 / 4);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
+
